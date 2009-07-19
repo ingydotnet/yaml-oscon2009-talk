3eed6fe714f9c66bcee5f96658c3141225a44c7e
Ingy döt Net <ingy@ingy.net>
2009-07-19 03:54:33 -0700

    Add php port

---
+++ Sample/dump-load.php
@@ -0,0 +1,24 @@
+<?php
+
+include("spyc.php");
+
+$object1 = array(
+    'name' => 'Ingy dot Net',
+    'number' => 42,
+    'needs' => array(
+        'sex',
+        'drugs',
+        'rock and roll'
+    )
+);
+
+$yaml1 = Spyc::YAMLDump($object1);
+$object2 = Spyc::YAMLLoad($yaml1);
+$yaml2 = Spyc::YAMLDump($object2);
+
+if ($yaml1 == $yaml2) {
+    echo $yaml1;
+}
+else {
+    throw new Exception($yaml1 . "!=\n" . $yaml2);
+}
